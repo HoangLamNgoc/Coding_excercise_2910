bed546452f3bc77f84bef7679af6f42b3c619ede
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-10 09:35:42 +0700

    Update and rename Task03 to Task03.cpp

--- BaiTap/Task03.cpp
+++ BaiTap/Task03.cpp
@@ -8,9 +8,9 @@ int main()
 {
   int n; double S=0;
   cin >> n ;
-	for ( int i=n; i>=2; --i)
+	for ( int i = n ; i >= 2 ; --i )
  	{ 
-               S= 1/(i+S); 
+        S= 1/(i+S); 
 	}
   double s=1+S;
  cout << s << endl ; 
