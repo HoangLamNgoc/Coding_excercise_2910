16e50e46e1798c58c54cb1bcaf10f462effbe4a7
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-10 22:13:04 +0700

    Update 3.cpp

--- ProjectEuler/3.cpp
+++ ProjectEuler/3.cpp
@@ -13,16 +13,16 @@ bool isPrime(long long x) {
 }
 
 int main() {
- cin >> n;
- vector<int> primeFactors; 
- for (long long i = 1; i * i <= n; ++i) {
-    if(n % i == 0) {    
-        long long j = n / i;
-        if(isPrime(i)) primeFactors.push_back(i);
-        if(isPrime(j)) primeFactors.push_back(j);   
+    cin >> n;
+    vector<int> primeFactors; 
+    for (long long i = 1; i * i <= n; ++i) {
+        if(n % i == 0) {    
+            long long j = n / i;
+            if(isPrime(i)) primeFactors.push_back(i);
+            if(isPrime(j)) primeFactors.push_back(j);   
+        }
     }
- } 
-cout << *max_element( primeFactors.begin(), primeFactors.end()); 
+    cout << *max_element( primeFactors.begin(), primeFactors.end()); 
     return 0 ;
 }
 
