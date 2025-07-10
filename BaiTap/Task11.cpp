cdf355c1b3c1cfbdf32fc51b91b644c6f75cc3e3
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-10 11:00:37 +0700

    Update Task11.cpp

--- BaiTap/Task11.cpp
+++ BaiTap/Task11.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
 
+using namespace std;
+
+int main()
+{
+    int f1 = 1 , f2 = 1 , fn ;
+    int n ;
+    cin >> n ;
+    for ( int i = 3 ; i <= n ; ++i)
+    {
+        fn = f1 + f2 ;
+        f1 = f2 ;
+        f2 = fn ;
+    }
+    cout << fn << endl;
+    return 0;
+}
