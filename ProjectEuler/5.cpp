fdfd7ab61db02a3f4f54f44e07aee0e116c348f8
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-10 22:03:28 +0700

    Update 5.cpp

--- ProjectEuler/5.cpp
+++ ProjectEuler/5.cpp
@@ -6,7 +6,7 @@
 using namespace std; 
 
 bool isDivisible(int n) {
-    if (i < 0) return false; 
+    if (n < 0) return false; 
     for ( int i = 1; i <= 20; ++i)
         if (n % i != 0) return false;
     return true;
@@ -15,7 +15,7 @@ bool isDivisible(int n) {
 int main() {
     int a=1;
     cin >> a; 
-    vector <int> Number ( int i) ;
+    vector <int> Number ;
     for(int i=a; ; ++i) {
         if (isDivisible(i)) { 
             Number.push_back(i);
