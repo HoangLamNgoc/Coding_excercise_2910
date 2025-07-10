9407bdafb6da636db57c50c4b4540e67b12cbf58
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-10 09:10:00 +0700

    Update 1.cpp

--- ProjectEuler/1.cpp
+++ ProjectEuler/1.cpp
@@ -2,14 +2,15 @@
 #include <cmath>
 #include <algorithm>
 using namespace std; 
-int main() {
-    int snt=0; int n ; 
+int main() 
+{
+    int snt = 0 ; int n ; 
     cin >> n ; 
-     for ( int b=1; b<n; ++b) 
+    for ( int b = 1 ; b < n ; ++b) 
     { 
-        if (b%5==0 || b%3==0) 
-        snt= snt+b;
+        if ( b % 5 == 0 || b % 3 == 0 ) 
+        snt = snt + b ;
     } 
-    cout <<snt << endl ; 
+    cout << snt << endl ; 
     return 0;
 }
