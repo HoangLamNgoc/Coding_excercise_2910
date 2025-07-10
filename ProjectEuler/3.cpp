d6eb3493bc8fb1800316544304d1d3b25add99f4
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-10 20:52:29 +0700

    Update 3.cpp

--- ProjectEuler/3.cpp
+++ ProjectEuler/3.cpp
@@ -2,37 +2,27 @@
 #include <cmath>
 #include <algorithm>
 #include <vector>
+
 using namespace std; 
 
-bool isPrime(int x)
-{
-    if (x<2) return false;
-    if( x == 2) return true ; 
-    if( x%2 == 0) return false ; 
-    for (int t=3;t * t <=x; ++t)
-        { 
-            if (x%t == 0) 
-            return false;
-        }
-        return true; 
+bool isPrime(long long x) {
+    if (x < 2) return false;
+    for (long long y = x; y * y <= x; ++y) 
+        if (x % y == 0) return false;
+    return true; 
 }
 
 int main() {
- long long n;
- cin >> n ; 
+ cin >> n;
  vector<int> primeFactors; 
- for (long long i=1; i * i <= n;++i)
- {
-    if(n%i==0)
-    {     long long j = n/i;
-           if(isPrime(i))
-               primeFactors.push_back(i);
-           if(isPrime(j))
-               primeFactors.push_back(j);   
+ for (long long i = 1; i * i <= n; ++i) {
+    if(n % i == 0) {    
+        long long j = n / i;
+        if(isPrime(i)) primeFactors.push_back(i);
+        if(isPrime(j)) primeFactors.push_back(j);   
     }
- }
-   long long maxval= *max_element( primeFactors.begin(), primeFactors.end());
-cout << maxval << endl; 
+ } 
+cout << *max_element( primeFactors.begin(), primeFactors.end()); 
     return 0 ;
 }
 
