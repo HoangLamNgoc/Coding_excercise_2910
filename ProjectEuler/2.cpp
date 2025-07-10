35e7d280df151b27309c5934f0ed0edc3bf06bbe
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-10 09:17:00 +0700

    Update 2.cpp

--- ProjectEuler/2.cpp
+++ ProjectEuler/2.cpp
@@ -6,11 +6,11 @@ int main() {
     int a=1, b=2; int n; int snt=2; 
     cin >> n ; 
     for ( int c = a+b ; c<=n ; c=a+b) {
-        c = a+b;
-        a=b;
-        b=c; 
-        if ( c%2==0)
-        snt= snt +c;
+        c = a + b;
+        a = b;
+        b = c; 
+        if ( c % 2 == 0)
+        snt = snt + c ;
     }
     cout << snt << endl ;
 return 0;
