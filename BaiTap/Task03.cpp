ebe43337b44bbb8ff165dd4a29f423ac15f3993d
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-11 22:50:50 +0700

    Update Task03.cpp

--- BaiTap/Task03.cpp
+++ BaiTap/Task03.cpp
@@ -2,17 +2,15 @@
 #include <iostream> 
 #include <cmath>
 #include <algorithm> 
+
 using namespace std; 
 
-int main()
-{
-  int n ; double S = 0;
-  cin >> n ;
-	for ( int i = n ; i >= 2 ; --i )
- 	{ 
+int main() {
+	int n ; double S = 0;
+	cin >> n ;
+	for ( int i = n ; i >= 2 ; --i ) 
         S= 1/( i + S ); 
-	}
-  double s = 1 + S ;
- cout << s << endl ; 
- return 0; 
+	double s = 1 + S ;
+ 	cout << s << endl ; 
+ 	return 0; 
  }
