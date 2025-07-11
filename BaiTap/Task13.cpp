530fefa8c0f3b4acbd91c51ace044d99dc73391b
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-11 23:01:16 +0700

    Update Task13.cpp

--- BaiTap/Task13.cpp
+++ BaiTap/Task13.cpp
@@ -6,5 +6,11 @@ int main () {
     int n; int tong = 0 ; 
     cin >> n ; 
     while ( n > 0 ) {
+		int i = n % 10 ; 
+        if ( i % 2 == 1 ) tong = tong + i; 
         n = n / 10 ; 
+	}
+	cout << tong ; 
+	return 0 ; 
+} 
         
