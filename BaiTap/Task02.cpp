194e529347735ef763284524ffee7bcfc78d82d0
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-11 22:51:31 +0700

    Update Task02.cpp

--- BaiTap/Task02.cpp
+++ BaiTap/Task02.cpp
@@ -1,16 +1,14 @@
 #include <iostream> 
 #include <cmath>
 #include <algorithm> 
+
 using namespace std; 
 
-int main()
-{
+int main() {
     int n; double S = 0; 
     cin >> n ; 
-  	for (int i = 1; i <= n ; ++i)
-    {  
+  	for (int i = 1; i <= n ; ++i)   
       	S = sqrt ( 2 + S ) ;
-    }
   	cout << S << endl; 
   	return 0 ; 
 }
