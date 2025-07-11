aa29d0984b3038798e325e98b95a3478e7d6c396
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-11 10:54:17 +0700

    Update 6.cpp

--- ProjectEuler/6.cpp
+++ ProjectEuler/6.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
 
+using namespace std; 
+
+int main() {
+    int n; int S = 0 ; A = 0; 
+    cin >> n ;
+    for (int i = 1; i <= n; ++i) { 
+        S = S + i * i ;
+        A = A + i * i + 2 * round(sqrt(A)) * i;
+    }
+    cout << A - S; 
+    return 0;
+}
+    
