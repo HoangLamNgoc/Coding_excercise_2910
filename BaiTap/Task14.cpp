f4decadfa2bbf8ad218a9cd44b1fab5e98ce0db6
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-11 23:26:54 +0700

    Create Task14.cpp

---
+++ BaiTap/Task14.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+int main () {
+	int n , dem = 0 , tong = 0 ;
+	cin >> n ; 
+ 	while (n > 0) {
+		dem = dem + 1 ; 
+		if ( dem % 2 == 0 ) tong = tong + n % 10 ;
+		n = n / 10 ;
+	}
+	cout << tong ; 
+	return 0; 
+}
+		
