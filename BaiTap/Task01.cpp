49c557ed07b41a708e242d98e4f020ff87573346
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-11 22:52:11 +0700

    Update Task01.cpp

--- BaiTap/Task01.cpp
+++ BaiTap/Task01.cpp
@@ -1,21 +1,20 @@
 #include <iostream> 
 #include <cmath>
 #include <algorithm> 
+
 using namespace std; 
 
-int main()
-{
+int main() {
     int n ; double S = 0 ; 
-  cin >> n ; 
-  for (int i = 1 ; i <= n ; ++i )
-  {
+    cin >> n ; 
+    for (int i = 1 ; i <= n ; ++i ) {
     if( i % 2 == 0)
         S= S-1/sqrt(i);
     else 
         S= S+1/sqrt(i); 
-  }
-cout << S << endl ; 
-return 0; 
+    }
+    cout << S << endl ; 
+    return 0; 
 }
     
       
