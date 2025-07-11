76f68643bb6649edbb0f3f8ecf0c243c2c7e0997
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-11 23:35:54 +0700

    Update Task14.cpp

--- BaiTap/Task14.cpp
+++ BaiTap/Task14.cpp
@@ -3,14 +3,14 @@
 using namespace std;  
 
 int main () {
-int n , dem = 0 , tong = 0 ;
-	cin >> n ;
- 	while (n > 0) {
-		dem = dem + 1 ; 
-		if ( dem % 2 == 0 ) tong = tong + n % 10 ;
-		n = n / 10 ;
-	}
-	cout << tong ; 
+    int n , dem = 0 , tong = 0 ;
+    cin >> n ;
+    while (n > 0) {
+	    dem = dem + 1 ; 
+	    if ( dem % 2 == 0 ) tong = tong + n % 10 ;
+	    n = n / 10 ;
+    }
+    cout << tong ; 
 	return 0; 
 }
 		
