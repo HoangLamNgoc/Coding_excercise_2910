b7afab812128b089fc2e0ef785247d10148c5ffb
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-12 22:44:31 +0700

    Update Task25.cpp

--- BaiTap/Task25.cpp
+++ BaiTap/Task25.cpp
@@ -5,21 +5,20 @@ using namespace std ;
 int doan_duong ( int n ) {
     int dem = 0 , maxval = 0 ; 
     while ( n > 0 ) {
-		int digit = n % 10 ; 
-		if ( digit != 0 ) {
-		  dem = dem + 1 ; 
-			if ( dem > maxval )
-				maxval = dem ; 
-		}
-		else dem = 0 ; 	
-		n = n / 10 ; 
+        int digit = n % 10 ; 
+	if ( digit != 0 ) {
+            dem = dem + 1 ; 
+	    if ( dem > maxval ) maxval = dem ; 
 	}
+	else dem = 0 ; 	
+	n = n / 10 ; 
+    }
     return maxval ; 
 }
     
 int main () {
-	int num ; 
-	cin >> num ; 
+    int num ; 
+    cin >> num ; 
     cout << doan_duong ( num ) ; 
     return 0 ; 
 }
