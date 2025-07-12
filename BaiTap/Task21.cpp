22d181e1418922404eeeaa159614ce00b807e5e1
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-12 10:36:58 +0700

    Update Task21.cpp

--- BaiTap/Task21.cpp
+++ BaiTap/Task21.cpp
@@ -1,1 +1,16 @@
+# include <bits/stdc++.h> 
 
+using namespace std ; 
+
+int main () {
+    int n , tong = 0 ; 
+    cin >> n ; 
+    for ( int i = 1 ; i * i <= n ; ++i) {
+       if ( n % i == 0 ) { 
+           if ( i == n / i ) tong = tong + i ; 
+           if ( i != n / i ) tong = tong + n / i + i ;
+       }
+    } 
+    cout << tong ; 
+    return 0 ; 
+}
