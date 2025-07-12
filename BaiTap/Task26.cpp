e5a1034a77b0c52e6c5fcb55d450ddfafe06a806
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-12 23:28:34 +0700

    Update Task26.cpp

--- BaiTap/Task26.cpp
+++ BaiTap/Task26.cpp
@@ -8,11 +8,11 @@ int duong_chay ( int n ) {
         int unit = n % 10 , tens = ( n / 10 ) % 10 ; 
 	    if ( unit <= tens ) {
             dem = dem + 1 ; 
-		    if ( dem > maxval ) {
-				maxval = dem ; 
+	        if ( dem > maxval ) {
+		        maxval = dem ; 
 	            count = 1 ; 
             }
-		else if ( dem == maxval ) count = count + 1 ; 
+            else if ( dem == maxval ) count = count + 1 ; 
 		}
         else dem = 0 ;
         n = n / 10 ; 
