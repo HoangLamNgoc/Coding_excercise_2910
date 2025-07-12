888055a6934ab9c7b8ccb7645dbd284c50f96d63
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-12 22:11:19 +0700

    Create Task24.cpp

---
+++ BaiTap/Task24.cpp
@@ -0,0 +1,27 @@
+# include <bits/stdc++.h>
+
+using namespace std ; 
+
+int doan_0 ( int n ) {
+    int dem = 0 , maxval = 0 , count = 0 ; 
+    while ( n > 0 ) {
+		int digit = n % 10 ; 
+		if ( digit == 0 ) {
+			dem = dem + 1 ; 
+			if ( dem > maxval ) {
+				maxval = dem ; 
+				count = 1 ; 
+			}
+			else if ( dem == maxval ) count = count + 1 ; 
+		else dem = 0 ; 	
+	}
+    return count ; 
+}
+    
+int main () {
+	int num ; 
+	cin >> num ; 
+    cout << doan_0 ( num ) ; 
+    return 0 ; 
+}
+    
