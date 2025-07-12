f81e384ec79ad7f86fdb9bf589494bba412401dc
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-12 22:40:33 +0700

    Create Task25.cpp

---
+++ BaiTap/Task25.cpp
@@ -0,0 +1,26 @@
+ # include <bits/stdc++.h>
+
+using namespace std ; 
+ 
+int doan_0 ( int n ) {
+    int dem = 0 , maxval = 0 ; 
+    while ( n > 0 ) {
+		int digit = n % 10 ; 
+		if ( digit != 0 ) {
+		  dem = dem + 1 ; 
+			if ( dem > maxval )
+				maxval = dem ; 
+		}
+		else dem = 0 ; 	
+		n = n / 10 ; 
+	}
+    return maxval ; 
+}
+    
+int main () {
+	int num ; 
+	cin >> num ; 
+    cout << doan_0 ( num ) ; 
+    return 0 ; 
+}
+    
