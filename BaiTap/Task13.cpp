5a365b5d2450251d9a44c48167572b0655e00fcd
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-13 14:52:57 +0700

    Update Task13.cpp

--- BaiTap/Task13.cpp
+++ BaiTap/Task13.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main () { 
     int n; int tong = 0 ; 
     cin >> n ; 
-    n = abs(n) 
+    n = abs(n) ;
     while ( n > 0 ) {
         int i = n % 10 ; 
         if ( i % 2 == 1 ) tong = tong + i; 
