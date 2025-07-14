133c0ae0ce122f7a32540f158fa7a0a6cd553bf3
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-14 10:14:31 +0700

    Update Task38.cpp

--- BaiTap/Task38.cpp
+++ BaiTap/Task38.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
 
+using namespace std; 
+
+bool isPrime(int n) {
+    for (int i = 2 ; i * i <= n; ++i) 
+        if (n % i == 0) return false;
+    return true; 
+}
+
+int main() {
+    cin >> num;
+    set<int> primeFactors; 
+    for (int i = 2 ; i * i <= num; ++i) {
+        if(num % i == 0) {    
+            int j = num / i;
+            if(isPrime(i)) primeFactors.insert(i);
+            if(isPrime(j)) primeFactors.insert(j);   
+        }
+    }
+    for (int n : primeFactors ) 
+        cout << n << " " ; 
+    return 0 ;
+}
