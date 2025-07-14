f168937831ab09ec9c3dfade385464559f8a3a94
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-14 09:57:31 +0700

    Create Task37.cpp

---
+++ BaiTap/Task37.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+bool isPrime ( int n ) {
+    if ( n < 2 ) return false ; 
+    for ( int i = 2 ; i * i <= n ; ++i ) 
+        if ( n % i == 0 ) return false ; 
+    return true ; 
+} 
+
+vector <int> PrimeFactors(int n) { 
+    vector <int> Factors ; 
+    for ( int i = 2 ; i * i <= n ; ++i ) { 
+        while ( n % i == 0 ) {
+            if (isPrime(i)) Factors.push_back(i) ; 
+            n = n / i ; 
+        }
+    }
+    if ( n > 1 ) Factors.push_back(n) ; 
+    return Factors; 
+}
+int main () {
+    int num ; 
+    cin >> num ; 
+    vector <int> Factors = PrimeFactors(num) ; 
+    for ( int x : Factors ) 
+        cout << x << " " ; 
+    cout << endl ; 
+    return 0 ; 
+}
