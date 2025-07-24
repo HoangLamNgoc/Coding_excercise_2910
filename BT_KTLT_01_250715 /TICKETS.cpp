b031b94ac0f8dcf34911422a744295809592e400
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-24 20:00:36 +0700

    Create TICKETS.cpp

---
+++ BT_KTLT_01_250715 /TICKETS.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long tienve ( long long n, long long k, long long p1, long long p2 ) { 
+    if ( k == 0) return n * p1 ; 
+    long long le = n * p1 ; 
+    long long si = ((n + k - 1) / k ) * p2 ; 
+    return min ( le, si ) ;     
+}
+
+int main() {
+    freopen ("TICKETS.INP" , "r", stdin) ; 
+    freopen ("TICKETS.OUT" , "w", stdout) ;
+    long long n, k, p1, p2 ;
+    cin >> n >> k >> p1 >> p2 ; 
+    long long so_tien_toi_thieu = tienve ( n, k, p1, p2 ) ;
+    cout << so_tien_toi_thieu; 
+    return 0;
+}
