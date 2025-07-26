9a342185957f47b0fc1689328a68132ff3d5fddf
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-26 22:56:38 +0700

    Update KTHDIV.cpp

--- BT_KTLT_01_250715 /KTHDIV.cpp
+++ BT_KTLT_01_250715 /KTHDIV.cpp
@@ -2,14 +2,15 @@
 
 using namespace std;
 
-vector<int> divisor (int n) {
-    vector<int> Divisor ;
+int divisor (int n, int k) {
+    int dem = 0 ; 
     for ( int i = 1 ; i <= n ; ++i ) {
-        if (n % i == 0 ) Divisor.push_back(i) ; 
-        if ( n % i == 0 && n / i == i ) Divisor.push_back(i) ; 
+        if (n % i == 0 ) { 
+            dem++; 
+            if ( dem == k ) return i ;
+        }
     }
-    sort(Divisor.begin(), Divisor.end());
-    return Divisor ; 
+    return -1 ; 
 }
 
 int main(){
@@ -17,7 +18,6 @@ int main(){
     freopen ( "KTHDIV.OUT", "w", stdout ) ;
     int n , k ; 
     cin >> n >> k ; 
-    vector<int> Divisor = divisor(n) ; 
-    cout << Divisor[k-1] ; 
+    cout << divisor(n, k) ; 
     return 0;
 }
