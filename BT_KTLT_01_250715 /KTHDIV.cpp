3ae6a9574ac1a2045b72bb5b75fde2ccdb3387c1
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-26 23:29:50 +0700

    Update KTHDIV.cpp

--- BT_KTLT_01_250715 /KTHDIV.cpp
+++ BT_KTLT_01_250715 /KTHDIV.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-int divisor (int n, int k) {
-    int dem = 0 ; 
-    for ( int i = 1 ; i <= n ; ++i ) {
-        if (n % i == 0 ) { 
-            dem++; 
-            if ( dem == k ) return i ;
+long long divisor (long long n, int k) {
+    vector<long long> uoc; 
+    for ( long long i = 1 ; i * i <= n ; ++i ) {
+        if (n % i == 0 ) {
+            uoc.push_back(i); 
+            if ( i != n / i ) uoc.push_back(n / i); 
         }
     }
+    sort (uoc.begin(), uoc.end()); 
+    if ( k <= uoc.size()) return uoc[k - 1];
     return -1 ; 
 }
 
 int main(){
     freopen ( "KTHDIV.INP", "r", stdin ) ;
     freopen ( "KTHDIV.OUT", "w", stdout ) ;
-    int n , k ; 
+    long long n , k ; 
     cin >> n >> k ; 
     cout << divisor(n, k) ; 
     return 0;
