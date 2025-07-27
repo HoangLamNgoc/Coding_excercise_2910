77b5945dcc676b1ac8b7da904b84778d46802bfc
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-28 01:16:04 +0700

    Create FRIDAY.cpp

---
+++ BT_KTLT_01_250715 /FRIDAY.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long So_ngay_xau ( int k, long long n) {
+    for ( int i = 1; i <= 7 ; ++i ) {
+        int thu = (k + 12) % 7; 
+        if (thu == 5) 
+            return (n - i) / 7 + 1 ;
+        k = (k + 30) % 7;
+    }
+    return 0 ; 
+}
+int main() {
+    freopen ( "FRIDAY.INP", "r" , stdin ); 
+    freopen ( "FRIDAY.OUT", "w" , stdout ); 
+    long long n; 
+    int k ; 
+    cin >> n >> k ; 
+    cout << So_ngay_xau(k, n); 
+    return 0;
+}
