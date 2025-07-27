0aa7a6088d9141c5f5a2658de11ee4cfe21327dc
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-07-27 22:35:26 +0700

    Update CALENDAR.cpp

--- BT_KTLT_01_250715 /CALENDAR.cpp
+++ BT_KTLT_01_250715 /CALENDAR.cpp
@@ -1,28 +1,28 @@
-#include<bits/stdc++.h>
+=#include<bits/stdc++.h>
 
 using namespace std;
 
-long long So_ngay (long long m , int d) {
-    long long songay = 0 ; 
-    if ( m % 2 != 0 ) songay = 30 * ( m - 1 )  + (( m - 1 ) / 2 ) + d ; 
-    else songay = 30 * ( m - 1 ) + ( ( m - 1 ) / 2 ) + 1 + d ;
+long long So_ngay (long long m , long long d) {
+    long long sochan = ( m - 1 ) / 2 ; 
+    long long sole = m / 2 ; 
+    long long songay = sochan * 30 + sole * 31 + d - 1; 
     return songay ; 
 }
 
 int Thu(long long songay , int w ) {
-    int dem = songay % 7 ; 
-    int thu = (dem + w - 1) % 7 ; 
+    int thu = (songay + w - 1) % 7 + 1;
+    if ( thu > 7 ) thu -= 7 ;
     return thu ; 
 }
 
-int main() {
+int main() {A
     freopen ( "CALENDAR.INP", "r" , stdin ); 
     freopen ( "CALENDAR.OUT", "w" , stdout ); 
     int w ; 
     long long m; 
     int d; 
     cin >> w ;
-    cin >> m >> d; 
+    cin >> d >> m; 
     int songay = So_ngay (m, d) ; 
     int ket_qua = Thu (songay, w) ; 
     cout << ket_qua ; 
