3d04823dd73ccdd0ecf9a6e08be6cd6d89e67f6c
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-06 00:46:34 +0700

    Create TrailingZero.cpp

---
+++ CSES/TrailingZero.cpp
@@ -0,0 +1,16 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    long long n, dem = 0 ;
+    cin >> n ;
+  
+    while ( n >= 5) {
+        dem += n / 5;
+        n /= 5;
+    }
+  
+    cout << dem ;
+    return 0 ;
+}
