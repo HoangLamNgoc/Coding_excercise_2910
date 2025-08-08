a8450af167c21133720b931d6d1455460a347911
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-08 15:27:56 +0700

    Create GrayCode.cpp

---
+++ CSES/GrayCode.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+void Sol(vector<string>& v, int num) {
+    if ( num == 0) {
+        for (const string& s : v)
+            cout << s << endl;
+        return;
+    }
+
+    vector<string> vec = v;
+    reverse(vec.begin(),vec.end());
+
+    vector <string> new_v;
+
+    for ( const string& s : v)
+        new_v.push_back(string ("0") + s);
+
+    for ( const string& s : vec)
+        new_v.push_back(string("1") + s);
+
+    Sol (new_v, num - 1);
+}
+
+
+int main () {
+    int n;
+    cin >> n;
+
+    vector<string> v;
+    v.push_back("");
+
+    Sol(v,n);
+    return 0;
+
+}
