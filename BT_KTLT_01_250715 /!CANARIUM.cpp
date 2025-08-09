9ec87d2263641b03084e69fa346c9fe825b3084a
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-09 11:03:48 +0700

    Update !CANARIUM.cpp

--- BT_KTLT_01_250715 /!CANARIUM.cpp
+++ BT_KTLT_01_250715 /!CANARIUM.cpp
@@ -1,42 +1,33 @@
-#include<bits/stdc++.h>
+#include <iostream> 
+#include <algorithm> 
 
-using namespace std;
+using namespace std; 
 
-void Sol ( long long k ) { 
-    long long tong = k * 2; 
-    long long minval = 1e12; 
-    long long m = -1 , n = -1;
-    
-    for (long long i = 0; i <= ( k - 1 ) / 2; ++i) { 
-        long long n1 = ( tong - 2 * i + 1) / ( 2 * ( i + 1));
-        
-        if ( abs(n1 - i) <= minval ) { 
-            minval = abs(n1 - i) ; 
-            if ( n1 > i) { 
-                n = n1; 
-                m = i;
-            }
-            
-            else { 
-                n = i;
-                m = n1; 
+
+void Sol(long long k) { 
+    long long m = -1, n = -1; 
+
+    for ( long long n1 = k / 2; n1 >= 2; --n1 ) { 
+        for ( long long i = n1 - 1; i >= 1; --i ) { 
+             long long k1 = i * n + (i + n + 1 ) / 2  ; 
+            if ( k == k1) { 
+                m = i; 
+                n = n1;
+                break; 
             }
         }
     }
-    
     cout << m << ' ' << n << endl;
-    return;
 }
 
 
-int main(){
-    freopen("CANARIUM.INP", "r", stdin); 
-    freopen("CANARIUM.OUT", "w", stdout); 
+int main () { 
+    freopen ("CANARIUM.INP", "r", stdin); 
+    freopen ("CANARIUM.OUT", "w", stdout);
     
     long long k; 
-    
-    while (cin >> k) 
-        Sol(k); 
-    
-    return 0;
+    while ( cin >> k) { 
+        Sol (k); 
+    }
+    return 0; 
 }
