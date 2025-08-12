18935d01cc28dd0224c200ee429dba7f06a026da
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-12 10:40:17 +0700

    Update !CONSEQ.cpp

--- BT_KTLT_01_250715 /!CONSEQ.cpp
+++ BT_KTLT_01_250715 /!CONSEQ.cpp
@@ -1,40 +1,50 @@
-#include <iostream> 
-#include <algorithm> 
+#include <iostream>
 
-using namespace std; 
+using namespace std;
 
-long long Conseq(long long w, long long m, long long k) {
-    long long result = 1; 
+long long Conseq(long long w, long long m, long long k) { 
+    long long dem = 0; 
+    long long cnt = 1; 
     
-    for ( long long i = m; i <= 1e16 ; ++i) {
-        long long dem = 0; 
-        long long cnt = 0;
-
-        long long j = i;
-        while ( j > 0) { 
-            j /= 10; 
-            dem++; 
-        }
-        
-        cnt += dem * k ; 
-        if ( cnt > w ) 
-            return result; 
-            
-        else if ( cnt == w ) 
-            return result + 1;
-            
-        else 
-            result++; 
+    long long copy_m = m;
+    while (copy_m > 0) {
+        dem++;
+        cnt *= 10; 
+        copy_m /= 10;
     }
-    return result; 
-}
 
+    
+    long long tong = 0;
+    long long s = 0; 
+    
+    while ( w > 0) { 
+        long long y = w / k / dem ; 
+        if ( y <= cnt - m ) { 
+            tong += y; 
+            break; 
+        } 
+        
+        else { 
+            s = cnt - m; 
+            tong += cnt - m; 
+            w = w - s * k * dem; 
+        } 
+        
+        m = cnt; 
+        cnt *= 10; 
+        ++dem; 
+    }
+    return tong; 
+} 
 
-int main() {
-    freopen ("CONSEQ.INP", "r", stdin); 
-    freopen ("CONSEQ.OUT", "w", stdout);
+    
+int main(){
+    freopen("CONSEQ.INP", "r", stdin); 
+    freopen("CONSEQ.OUT", "w", stdout); 
+    
     long long w, m, k; 
     cin >> w >> m >> k; 
-    long long res = Conseq ( w, m, k); 
-    cout << res << endl; 
+    
+    cout << Conseq(w, m, k) << endl; 
+    return 0;
 }
