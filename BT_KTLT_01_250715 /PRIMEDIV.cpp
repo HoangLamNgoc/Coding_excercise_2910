8992de8e46e806c37ae1118d1ad30c6a56bfa89a
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-12 16:25:30 +0700

    Update PRIMEDIV.cpp

--- BT_KTLT_01_250715 /PRIMEDIV.cpp
+++ BT_KTLT_01_250715 /PRIMEDIV.cpp
@@ -2,14 +2,6 @@
 
 using namespace std;
 
-bool isPrime( long long n) { 
-    if ( n < 2 ) return false; 
-    for ( long long i = 2; i * i <= n; ++i) {
-        if ( n % i == 0 ) return false;
-    }
-    return true; 
-}
-
 int main(){
     freopen("PRIMEDIV.INP", "r", stdin); 
     freopen("PRIMEDIV.OUT", "w", stdout); 
@@ -20,12 +12,8 @@ int main(){
     set <long long> v; 
     
     for (long long i = 1; i * i <= n; ++i) { 
-        if ( isPrime(i) && n % i == 0 ) {
+        if ( n % i == 0 ) {
             v.insert(i);
-        
-            long long j = n / i; 
-            if ( i != j && isPrime(j)) 
-                v.insert(j); 
             while ( n % i == 0 ) 
                 n /= i;
         }
