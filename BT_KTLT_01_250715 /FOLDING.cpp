564ed1196589492f5fd2a322cd86a430f8cde9e4
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-12 14:26:25 +0700

    Update FOLDING.cpp

--- BT_KTLT_01_250715 /FOLDING.cpp
+++ BT_KTLT_01_250715 /FOLDING.cpp
@@ -4,16 +4,15 @@ using namespace std;
 
 long long fold(long long i, long long j) { 
     long long cnt = 0; 
-    bool flag = true;
     
-    if ( j > i ) flag = false; 
-    
-    while ( i > 2 * j && flag) {
-        i /= 2; 
+    if ( j > i ) return -1;  
+    if ( i == j) return 0; 
+    while ( i > j ) {
+        i = (i + 1) / 2; 
         cnt++; 
     } 
-    if (!flag) return cnt = 0; 
-    else return cnt + 1; 
+    
+    return cnt; 
 } 
     
 int main() { 
@@ -23,12 +22,16 @@ int main() {
     long long W, H, w, h; 
     cin >> W >> H >> w >> h; 
     
-    long long cnt = fold(W, w); 
-    long long dem = fold(H,h); 
-    
-    if ( cnt == 0 || dem == 0)  
-        cout << -1; 
-    else 
-        cout << cnt + dem << endl;
-    return 0; 
+    long long res = 1e9; 
+
+    long long a1 = fold(W, w), b1 = fold(H, h);
+    if (a1 != -1 && b1 != -1) res = min(res, a1 + b1);
+
+    long long a2 = fold(W, h) , b2 = fold(H, w);
+    if (a2 != -1 && b2 != -1) res = min(res, a2 + b2);
+
+    if (res == 1e9) cout << -1;
+    else cout << res;
+
+    return 0;
 }
