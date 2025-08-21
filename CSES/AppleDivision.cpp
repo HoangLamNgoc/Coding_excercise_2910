32e9e3289f458cb0745d15571659f5e77201d5d5
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-21 17:41:05 +0700

    Update and rename !AppleDivision(sai)+).cpp to AppleDivision.cpp

---
+++ CSES/AppleDivision.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+long long minval = 1e6;
+int n; 
+int a[25]; 
+
+void Sol(int i, long long sum, long long total) {
+	if ( i == n ) {
+		int diff = (long long)abs(2 * sum - total ); 
+		minval = min(diff, minval); 
+		return; 
+	}
+		
+	Sol(i + 1, sum);
+	Sol ( i + 1; sum + a[i]); 
+}
+
+int main() {
+	cin >> n; 
+	long long total = 0; 
+	for ( int i = 0; i < n; ++i) {
+		cin >> a[i]; 
+		total += a[i]; 
+	}
+	Sol (0,0,total); 
+	cout << minval << endl; 
+}
