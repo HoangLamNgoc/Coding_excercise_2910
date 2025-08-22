4c0179ec5889828107f5999a72ad499f20ef188e
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-08-22 14:30:12 +0700

    Update AppleDivision.cpp

--- CSES/AppleDivision.cpp
+++ CSES/AppleDivision.cpp
@@ -1,3 +1,4 @@
+//ĐỆ QUY : 
 #include <iostream>
 #include <vector>
 
@@ -27,4 +28,35 @@ int main() {
 	Sol (0,0,total); 
 	cout << minval << endl; 
 	return 0;
+}
+
+// TOÁN TỬ BIT 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long a[n];
+    long long total = 0;
+    for ( int i = 0; i < n; ++i) {
+        cin >> a[i];
+        total += a[i];
+    }
+
+    long long dif = 1e9;
+    for ( int mask = 0; mask < ( 1 << n ) ; ++mask) {
+        long long sum = 0;
+        for ( int j = 0; j < n; ++j) {
+            if ( mask & ( 1 << j )) {
+                sum += a[j];
+                dif = min( dif,abs(total - 2 * sum));
+            }
+        }
+    }
+    cout << dif << endl;
+	return 0; 
 }
