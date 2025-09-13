38c6056b80cecf2cf3a2f8418cfe4338878f1735
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-09-13 23:51:56 +0700

    Create Grid Path.cpp

---
+++ CSES/Grid Path.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std; 
+
+const int N = 7; 
+const pair<int,int> a[4] = {{-1,0},{1,0},{0,1},{0,-1}};
+const string s = "UDRL";
+bool long long visited[N][N];
+long long ans; 
+
+bool empty(int x, y) {
+    return !visited[x][y] && 
+    
+void dfs(int x, int y) {
+    if ( x == N - 1 && y == 0 ){
+          for(int i = 0; i < n; ++i)
+              for(int j = 0; j < n; ++j) 
+                    if(visit[i][j] return;
+          ++ans; 
+          return;
+    }
+
+    if(x >= 1 &&
+
+                    
+    if(
