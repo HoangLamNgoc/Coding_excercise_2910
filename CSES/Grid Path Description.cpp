460dd246eda9d59d50b1cfbaa1efc798a4ff0097
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-09-15 15:42:44 +0700

    Update Grid Path Description.cpp

--- CSES/Grid Path Description.cpp
+++ CSES/Grid Path Description.cpp
@@ -4,8 +4,8 @@
 
 using namespace std; 
 
-const int N = 1e5; 
-int id = 0;
+const int N = 7; 
+string s;
 const pair<int,int> a[4] = {{-1,0},{1,0},{0,1},{0,-1}};
 const string rule = "UDRL";
 bool visited[N][N];
@@ -15,44 +15,39 @@ bool empty(int x,int y) {
     return !visited[x][y] && max(x, y) < N && min(x,y) >= 0; 
 }
     
-void dfs(int x, int y) {
+void dfs(int x, int y, int i) {
     if ( x == N - 1 && y == 0 ){
-          for(int i = 0; i < n; ++i)
-              for(int j = 0; j < n; ++j) 
-                    if(visit[i][j]) return;
-          ++ans; 
-          return;
+        if( i == 48) {
+              ++ans; 
+              return;
+        }
     }
+    
     visited[x][y] = true; 
-    if(visited[x - 1][y] && !visited[x][y - 1] && !visited[x][y + 1] && !visited[x + 1][y]) return;  
+    if(x >= 1 && y >= 1 && visited[x - 1][y] && !visited[x][y - 1] && !visited[x][y + 1] && !visited[x + 1][y]) return;  
 
-    if(visited[x][y - 1] && visited[x][y + 1] && !visited[x - 1][y] && !visited[x + 1][y]) return; 
+    if(x >= 1 && y >= 1 && visited[x][y - 1] && visited[x][y + 1] && !visited[x - 1][y] && !visited[x + 1][y]) return; 
 
-    if(visited[x - 1][y] && visited[x + 1][y] && !visited[x][y - 1] && !visited[x][y + 1]) return; 
+    if(x >= 1 && y >= 1 && visited[x - 1][y] && visited[x + 1][y] && !visited[x][y - 1] && !visited[x][y + 1]) return; 
 
-    for(int  i = 0; i < 4; ++i) {
-        int r = x + a[i].first; 
-        int c = y + a[i].second;
+
+    for(int  j = 0; j < 4; ++j) {
+        if( s[i] != '?' && s[i] != rule[j] )
+            continue;
+        int r = x + rule[j].first; 
+        int c = y + rule[j].second;
         if (!visited[r][c]) {
-            dfs(r, c);
-            visited[x][y] = false; 
+            dfs(r, c, i + 1);
+            visited[r][c] = false; 
         }
     }
 }
 
 
-int main() {
-    string s; 
+int main() { 
     cin >> s; 
-
-    for(int i = 0; i < 9; ++i) {
-        visited[0][i] = true; 
-        visited[8][i] = true;
-        visited[i][0] = true;
-        visited[i][8] = true; 
-    }
-
-    dfs(1,1); 
+    dfs(0, 0, 0); 
+    cout << ans << endl; 
     return 0; 
 }
     
