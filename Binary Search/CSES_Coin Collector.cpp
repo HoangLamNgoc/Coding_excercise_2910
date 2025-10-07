127bf8cfd126a30ca0a3880919d14e38d37fdaf8
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-07 22:40:52 +0700

    Update CSES_Coin Collector.cpp

--- Binary Search/CSES_Coin Collector.cpp
+++ Binary Search/CSES_Coin Collector.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <algorithm>
-#include <set> 
-
-using namespace std;
 
 int main() {
-    int n; cin >> n; 
+    int n; 
+    cin >> n; 
 
-    int a[n];
-    int dem = 0; 
-    for(int i = 0; i < n; ++i) cin >> a[i]; 
- 
-    multiset<int> s; 
-    for(int i = 0; i < n; ++i) {
-        auto it = s.lower_bound(a[i]);
-        if (it == s.begin()) s.insert(a[i]); 
-        else {
-            --it; 
-            s.erase(it); 
-            s.insert(a[i]); 
-        } 
+    int pos[n + 5]; 
+    for(int i = 1; i <= n; ++i ) {
+        int x; cin >> x; 
+        pos[x] = i; 
     }
+    int r = 0; 
+    for (int i = 2; i < n; ++i) 
+        if (pos[i] < pos[i - 1]) r++; 
 
-    cout << s.size() << endl; 
+    cout << r << endl; 
     return 0; 
 }
