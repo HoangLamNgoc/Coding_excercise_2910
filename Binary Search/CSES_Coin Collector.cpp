13085428be07c6ccb68ffbf51f33509100c66974
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-07 21:59:25 +0700

    Update CSES_Coin Collector.cpp

--- Binary Search/CSES_Coin Collector.cpp
+++ Binary Search/CSES_Coin Collector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <set> 
 
 using namespace std;
 
@@ -9,28 +10,18 @@ int main() {
     int a[n];
     int dem = 0; 
     for(int i = 0; i < n; ++i) cin >> a[i]; 
-
-    int i1 = 0; 
-    bool used[n];
-    fill_n(used, n, 0); 
-
-    while (i1 < n) {
-        int i = i1; 
-        while (used[i]) ++i;
-        if (i == n) break;
-        used[i] = true; 
-        int j = a[i];   
-        while (i < n - 1) {
-            if (j < a[i + 1] && !used[i + 1]) {
-                used[i + 1] = true;
-                j = a[i + 1];  
-            } 
-            ++i; 
-        }
-        ++dem; 
-        if (i == n) break;
-        while(used[i1]) i1++; 
+ 
+    multiset<int> s; 
+    for(int i = 0; i < n; ++i) {
+        auto it = s.lower_bound(a[i]);
+        if (it == s.begin()) s.insert(a[i]); 
+        else {
+            --it; 
+            s.erase(it); 
+            s.insert(a[i]); 
+        } 
     }
-    cout << dem << endl; 
+
+    cout << s.size() << endl; 
     return 0; 
 }
