64fefca368f485a452b7d214d0bfcb00771b6c0a
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-07 18:54:26 +0700

    Create CSES - Res Cus.cpp

---
+++ Binary Search/CSES - Res Cus.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int n; cin >> n;
+
+    pair<int,int> a[n + 5]; 
+    for(int i = 0; i < n; ++i) cin >> a[i].first >> a[i].second; 
+
+    vector<pair<int,int>> a2; 
+    for(int i = 0; i < n; i += 2) {
+        a2.push_back({a[i].first, 1}); 
+        a2.push_back({a[i].second, -1});  
+    }
+
+    sort(a2.begin(), a2.end()); 
+    int maxs = 0, cur = 0; 
+
+    for(auto& i : a2) {
+        cur += i.second;
+        maxs = max(cur, maxs);  
+    } 
+
+    cout << maxs << endl;
+    return 0; 
+}
