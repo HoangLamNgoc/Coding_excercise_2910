f6c5efab927b8e6ce4e15e64a4173fb6e241d0b9
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-11 13:14:58 +0700

    Create CSES - Course Schedule.cpp

---
+++ Topological Sort/CSES - Course Schedule.cpp
@@ -0,0 +1,58 @@
+#include <queue>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+const int maxn = 1e5 + 5; 
+vector<int> g[maxn], tp; 
+int a[maxn], vis[maxn]; 
+int n, m;
+bool ok = true; 
+
+void input() {
+    cin >> n >> m; 
+
+    while (m--) {
+        int u, v; 
+        cin >> u >> v; 
+        g[u].push_back(v); 
+    }
+
+    fill_n(vis, maxn, false); 
+    fill_n(a, maxn, 0); 
+}
+
+void dfs(int u) {
+    vis[u] = 1; 
+    for(int v : g[u]) {
+        if(!vis[v]) dfs(v);
+        else if (vis[v] == 1 ) {
+            cout << "IMPOSSIBLE" << endl;
+            ok = false;
+            exit(0);
+        }
+    }
+    tp.push_back(u);
+    vis[u] = 2;
+}
+
+int main() {
+    input();
+
+    for(int i = 1; i <= n; ++i) {
+        if(!vis[i]) dfs(i); 
+    }
+
+    if (!ok) return 0; 
+    if (tp.size() < n) {
+        cout << "IMPOSSIBLE" << endl; 
+        return 0; 
+    }
+
+    reverse(tp.begin(), tp.end());
+    int cnt = 0; 
+    for(int x : tp) a[x] = ++cnt; 
+    for(int i = 1; i <= n; ++i) cout << a[i] << ' '; 
+    return 0; 
+}
