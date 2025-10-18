e16a53db7c75285924b34d76ba38f5a381368681
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-18 08:23:12 +0700

    Update Dijkstra chuẩn - Kattis - shortest path1.cpp

--- Dijsktra/Dijkstra chuẩn - Kattis - shortest path1.cpp
+++ Dijsktra/Dijkstra chuẩn - Kattis - shortest path1.cpp
@@ -8,17 +8,21 @@ using namespace std;
 const int N = 1e5 + 5;
 const long long INF = 1e18;
 
-vector<pair<int,int>> g[N];
+vector<pair<int, int>> g[N];
 long long dist[N];
 
 void dijkstra(int s) {
-    priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> q;
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> q;
     fill(dist, dist + N, INF);
     dist[s] = 0;
     q.push({0, s});
 
     while (!q.empty()) {
-        auto [du, u] = q.top(); q.pop();
+        pair<long long,int> r = q.top();
+        q.pop();
+
+        long long du = r.first; 
+        int u = r.second; 
         if (du != dist[u]) continue;
 
         for (auto [v, w] : g[u]) {
@@ -31,20 +35,33 @@ void dijkstra(int s) {
 }
 
 int main() {
-    int n, m, k, t;
-    cin >> n >> m >> k >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    for (int i = 0; i < m; i++) {
-        int u, v, w1;
-        cin >> u >> v >> w1;
-        g[u].push_back({v, w1});
+    while (true) {
+        int n, m, q, s;
+        cin >> n >> m >> q >> s;
+        if (n == 0 && m == 0 && q == 0 && s == 0) break;
+
+        for (int i = 0; i < n; i++) g[i].clear();
+
+        for (int i = 0; i < m; i++) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            g[u].push_back({v, w});
+        }
+        dijkstra(s);
+        while (q--) {
+            int p;
+            cin >> p;
+            if (dist[p] < INF)
+                cout << dist[p] << '\n';
+            else
+                cout << "Impossible\n";
+        }
+
+        cout << '\n'; 
     }
-
-    dijkstra(k);
-
-    while (t--) {
-        int p;
-        cin >> p;
-        cout << dist[p] << '\n';
-    }
+    return 0;
 }
+
