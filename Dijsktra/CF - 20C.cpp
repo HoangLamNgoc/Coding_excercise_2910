d431e804410c7c2b6d0dd8c7a4ee9f089af83191
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-18 23:53:35 +0700

    Update CF - 20C.cpp

--- Dijsktra/CF - 20C.cpp
+++ Dijsktra/CF - 20C.cpp
@@ -3,34 +3,35 @@
 #include <vector>
 #include <queue>
 
-using namespace std; 
+using namespace std;
+
 const int N = 1e5 + 5;
-const long long INF = 1e18; 
+const long long INF = 2e18*1LL;
 
-vector<pair<int,int>> g[N];
-priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> q; 
+vector<pair<int, long long>> g[N];
 long long dist[N];
-int trace[N]; 
+int trace[N];
 
-void dij(int s) {
-    fill_n(dist, N, INF); 
-    fill_n(trace, N, 0); 
-    dist[s] = 0; 
-    trace[s] = -1; 
+void dij(int s, int n) {
+    fill_n(dist, N, INF);
+    fill_n(trace, N, -1);
+    vector<bool> used(N, false);
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> q;
+
+    dist[s] = 0;
     q.push({0, s});
 
-    while(!q.empty()) {
-        pair<long long,int> r = q.top(); q.pop(); 
-        int u = r.second; 
-        long long du = r.first; 
+    while (!q.empty()) {
+        auto [du, u] = q.top();
+        q.pop();
 
-        if(du != dist[u]) continue; 
-        for(pair<long long,int> i : g[u]) {
-            long long v = i.first; 
-            int w = i.second; 
-            if(dist[v] > dist[u] + w) {
-                trace[v] = u; 
-                dist[v] = dist[u] + w;  
+        if (used[u]) continue;
+        used[u] = true;
+
+        for (auto [v, w] : g[u]) {
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                trace[v] = u;
                 q.push({dist[v], v});
             }
         }
@@ -41,17 +42,22 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    while(m--) {
-        int u, v, w; 
-        cin >> u >> v >> w; 
-        g[u].push_back({v, w}); 
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        long long w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
     }
 
-    dij(1); 
-    vector<long long> path; 
-    for(int v = n; v != -1; v = trace[v]) 
-        path.push_back(v); 
-    reverse(path.begin(), path.end()); 
-    for(const long long& i : path) cout << i << ' '; 
-    return 0; 
+    dij(1, n);
+
+    vector<int> path;
+    for (int v = n; v != -1; v = trace[v])
+        path.push_back(v);
+    reverse(path.begin(), path.end());
+
+    if((int)path.size() > 1)for (int v : path) cout << v << ' ';
+    else cout << -1 << endl;
+    return 0;
 }
