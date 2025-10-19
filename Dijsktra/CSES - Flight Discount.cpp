91ff5086ee03328c0609a0d5d60ffe63e0d51ed0
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-10-19 11:50:44 +0700

    Update CSES - Flight Discount.cpp

--- Dijsktra/CSES - Flight Discount.cpp
+++ Dijsktra/CSES - Flight Discount.cpp
@@ -4,46 +4,48 @@
 #include <queue>
 
 using namespace std;
-const int N = 1e5; 
-const long long INF = 1LL * 2e18; 
-vector<pair<int,int>> g[N]; 
+const int N = 1e6 + 5;
+const long long INF = 1LL * 2e18;
+vector<pair<int,int>> g[N];
 long long d[N][2];
-int n, m;  
+int n, m;
 
 void dij(int s) {
-    vector<bool> p(N, 0); 
-    d[s][0] = d[s][1] = 0; 
+    bool p[N][2];
 
-    priority_queue<pair<long long,pair<int,int>>, vector<pair<long long,pair<int,int>>>, greater<pair<long long, pair<int,int>>>> q;  
-    q.push({0,{s, 0}}); 
+    for(int i = 1; i <= n; ++i) p[n][0] = p[n][1] = false;
+    d[s][0] = 0;
+
+    priority_queue<pair<long long,pair<int,int>>, vector<pair<long long,pair<int,int>>>, greater<pair<long long, pair<int,int>>>> q;
+    q.push({0,{s, 0}});
 
     while(!q.empty()) {
         pair<long long,pair<int,int>> r = q.top();
-        q.pop(); 
+        q.pop();
 
         int u = r.second.first;
-        int used = r.second.second; 
+        int used = r.second.second;
 
-        if(p[u]) continue;
-        p[u] = 1; 
+        if(p[u][used]) continue;
+        p[u][used] = 1;
 
         for(pair<int,int> c : g[u]) {
-            int v = c.first; 
-            int w = c.second; 
+            int v = c.first;
+            int w = c.second;
 
             if(!used && d[v][0] > d[u][0] + w) {
                 d[v][0] = d[u][0] + w;
-                q.push({d[v][0], {v, 0}});   
+                q.push({d[v][0], {v, 0}});
             }
 
-            if(!used && d[v][0] > d[u][0] + w / 2) {
-                d[v][1] = d[u][0] + w / 2; 
-                q.push({d[v][1], {v, 1}}); 
+            if(!used && d[v][1] > d[u][0] + w / 2) {
+                d[v][1] = d[u][0] + w / 2;
+                q.push({d[v][1], {v, 1}});
             }
 
             if(used && d[v][1] > d[u][1] + w) {
                 d[v][1] = d[u][1] + w;
-                q.push({d[v][1], {v, 1}}); 
+                q.push({d[v][1], {v, 1}});
             }
         }
     }
@@ -54,17 +56,17 @@ void input() {
 
     while(m--) {
         int u, v, w;
-        cin >> u >> v >> w; 
-        g[u].push_back({v, w}); 
-    } 
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+    }
 
-    for(int i = 1; i <= n; ++i) 
+    for(int i = 1; i <= n; ++i)
         d[i][1] = d[i][0] = INF;
-    dij(1);  
+    dij(1);
 }
 
 int main() {
-    input(); 
-    cout << min(d[n][0], d[n][1]) << '\n'; 
-    return 0; 
+    input();
+    cout << min(d[n][0], d[n][1]) << '\n';
+    return 0;
 }
