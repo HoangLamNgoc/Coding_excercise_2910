4209e9833d871b5bb32ea17471d741f18b086623
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-11-03 20:23:06 +0700

    Create Graph - Cycle Finding.cpp

---
+++ CSES/Graph - Cycle Finding.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std; 
+const int N = (int)25e2 + 5; 
+const int M = (int)5e3 + 5; 
+const int oo = (int)2e9 + 5; 
+struct edge {
+    int u, v, w; 
+
+    edge() {}; 
+    edge(int a, int b, int c) : u(a), v(b), w(c) {}; 
+}; 
+
+vector<edge> e(M);
+vector<int> path; 
+long long d[N]; 
+int trace[N]; 
+int n, m; 
+
+void bellmanford(int s) {
+    fill_n(d, N, oo); 
+    fill_n(trace, N, -1); 
+
+    d[s] = 0; 
+    for(int i = 1; i < n; ++i) {
+        for(edge E : e) {
+            int u = E.u, v = E.v, w = E.w; 
+            if (d[v] > d[u] + w && d[u] != oo) {
+                d[v] = d[u] + w; 
+                trace[v] = u; 
+            }
+        }
+    }
+}
+
+bool NegCy() {
+    int negst = -1; 
+
+    for(edge E : e) {
+        int u = E.u, v = E.v, w = E.w; 
+        if (d[v] > d[u] + w && d[u] != oo) {
+            trace[v] = u; 
+            negst = u; 
+            break; 
+        }
+    }
+
+    if (negst == -1) return 0; 
+    int u = negst;
+
+    for(int i = 0; i < n; ++i) u = trace[u]; 
+
+    path.emplace_back(u); 
+    for(int v = trace[u]; v != u; v = trace[v]) 
+        path.emplace_back(v); 
+    path.emplace_back(u); 
+    reverse(path.begin(), path.end()); 
+    return 1; 
+}   
+
+signed main() {
+    cin >> n >> m; 
+
+    for(int i = 1; i <= m; ++i) {
+        int u, v, w; 
+        cin >> u >> v >> w; 
+        e.emplace_back(edge(u, v, w)); 
+    }
+    bellmanford(1); 
+    if (NegCy()) {
+        cout << "YES" << '\n'; 
+        for(const int& i : path) cout << i << ' '; 
+    }
+    else cout << "NO" << '\n'; 
+}
