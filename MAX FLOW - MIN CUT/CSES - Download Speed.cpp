94132ccf2a3561089e501f53f9904fc622ae142e
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-11-09 16:43:18 +0700

    Update CSES - Download Speed.cpp

--- MAX FLOW - MIN CUT/CSES - Download Speed.cpp
+++ MAX FLOW - MIN CUT/CSES - Download Speed.cpp
@@ -1,82 +1,80 @@
 #include <iostream>
 #include <algorithm>
-#include <vector> 
+#include <vector>
 #include <queue>
 
-using namespace std; 
-int n, m, s = 1, t; 
+using namespace std;
+
+int n, m, s = 1, t;
 const int maxn = 501;
-const int inf = 1e9 + 7;  
+const long long inf_flow = 1e18;
+const int inf_d = 1e9;
 
-int c[maxn][maxn], f[maxn][maxn], d[maxn];
-
+long long c[maxn][maxn], f[maxn][maxn];
+int d[maxn];
 int curvertexid[maxn];
-vector<int> g[maxn];  
-
-int maxflow = 0; 
+vector<int> g[maxn];
+long long maxflow = 0;
 
 void bfs() {
-    fill(d, d + n + 1, inf); 
-
-    queue<int> q; 
-    q.push(s); 
-    d[s] = 0; 
+    fill(d, d + n + 1, inf_d);
+    queue<int> q;
+    q.push(s);
+    d[s] = 0;
 
     while(!q.empty()) {
-        int u = q.front(); q.pop(); 
-
+        int u = q.front(); q.pop();
         for(int v : g[u]) {
-            if (f[u][v] == c[u][v]) continue; 
-            if (d[v] != inf) continue; 
-
-            d[v] = d[u] + 1; 
-            q.push(v); 
+            if(f[u][v] == c[u][v]) continue;
+            if(d[v] != inf_d) continue;
+            d[v] = d[u] + 1;
+            q.push(v);
         }
     }
 }
 
-int dfs(int u, int curdel) {
-    if (curdel == 0) return 0; 
-
-    if (u == t) return curdel; 
+long long dfs(int u, long long curdel) {
+    if(curdel == 0) return 0;
+    if(u == t) return curdel;
 
     for(; curvertexid[u] < g[u].size(); ++curvertexid[u]) {
-        int v = g[u][curvertexid[u]]; 
+        int v = g[u][curvertexid[u]];
+        if(f[u][v] == c[u][v]) continue;
+        if(d[v] != d[u] + 1) continue;
 
-        if (f[u][v] == c[u][v]) continue; 
-        if (d[v] != d[u] + 1) continue; 
+        long long del = dfs(v, min(curdel, c[u][v] - f[u][v]));
+        if(del == 0) continue;
 
-        int del = dfs(v, min(curdel, c[u][v] - f[u][v]));
-
-        if (del == 0) continue; 
-
-        f[u][v] += del; 
-        f[v][u] -= del; 
-        return del;  
+        f[u][v] += del;
+        f[v][u] -= del;
+        return del;
     }
-    return 0; 
+    return 0;
 }
 
-signed main() {
-    cin >> n >> m; 
-    t = n; 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int u, v; 
-    while(m--) {
-        cin >> u >> v; 
-        cin >> c[u][v]; 
-        g[u].emplace_back(v); 
-        g[v].emplace_back(u); 
+    cin >> n >> m;
+    t = n;
+
+    for(int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        c[u][v] += w;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
 
     while(true) {
-        bfs(); 
+        bfs();
+        if(d[t] == inf_d) break;
         fill(curvertexid, curvertexid + n + 1, 0);
-        if (d[t] == inf) break; 
-        while (int del = dfs(s, inf)) {
-            maxflow += del; 
+        while(long long del = dfs(s, inf_flow)) {
+            maxflow += del;
         }
     }
-    cout << maxflow; 
-    return 0; 
+
+    cout << maxflow;
 }
