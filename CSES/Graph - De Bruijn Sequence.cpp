840d3708b209d2eba6bfb4e118836df4e1146a5a
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-11-13 16:01:18 +0700

    Update Graph - De Bruijn Sequence.cpp

--- CSES/Graph - De Bruijn Sequence.cpp
+++ CSES/Graph - De Bruijn Sequence.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <stack>
 
-using namespace std; 
+using namespace std;
 
 int main() {
-    int n; cin >> n; 
+    int n; cin >> n;
 
     if (n == 1) {
-        cout << "10"; 
-        return 0; 
+        cout << "10";
+        return 0;
     }
-    vector<vector<int>> g(1 << (n - 1)); 
+    vector<vector<int>> g(1 << (n - 1));
 
 
-    for(int i = 0; i <= (1 << (n - 1)); ++i) {
-        int son = (i << 1) % (1 << (n - 1)); 
+    for(int i = 0; i < (1 << (n - 1)); ++i) {
+        int son = (i << 1) % (1 << (n - 1));
 
         g[i].push_back(son);
-        g[i].push_back(son | 1); 
+        g[i].push_back(son | 1);
     }
 
     stack<int> todo;
-    vector<int> path; 
+    vector<int> path;
     todo.push(0);
 
     while(!todo.empty()) {
-        int u = todo.top(); 
+        int u = todo.top();
 
         if(!g[u].empty()) {
-            cout << g[u].back(); 
-            todo.push(g[u].back()); 
-            g[u].pop_back(); 
+            todo.push(g[u].back());
+            g[u].pop_back();
         }
         else {
-            todo.pop(); 
-            path.push_back(u & 1); 
+            todo.pop();
+            path.push_back(u & 1);
         }
-    } 
+    }
 
-    for(int i = 1; i < n - 2; ++i) path.push_back(0); 
-    for(int i : path) cout << i; 
+    for(int i = 1; i <= n - 2; ++i) path.push_back(0);
+    for(int i : path) cout << i;
 }
