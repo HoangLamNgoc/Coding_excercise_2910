e14a04e6937b76f8d4c952574a029a2c932d7def
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-11-17 10:57:49 +0700

    Update CSES - Counting Paths.cpp

--- Tree/CSES - Counting Paths.cpp
+++ Tree/CSES - Counting Paths.cpp
@@ -3,31 +3,32 @@
 #include <queue>
  
 using namespace std; 
-const int log = 20; 
-const int N = 2e5 + 5; 
+const int LOG = 20; 
+const int MAX_NODES = 2e5 + 5;
+const int oo = 1e9 + 5; 
  
-int up[N][log];
-vector<int> g[N];
-int h[N];
-bool vis[N]; 
+int up[MAX_NODES][LOG];
+vector<int> adj[MAX_NODES];
+int hei[MAX_NODES];
+bool vis[MAX_NODES]; 
  
-void bfs(int s) {
-    fill_n(vis, N, 0); 
-    fill_n(h, N, N); 
+void bfs(int src) {
+    fill_n(vis, MAX_NODES, 0); 
+    fill_n(hei, MAX_NODES, oo); 
  
     queue<int> q;    
     vis[s] = 1; 
-    h[s] = 0;
-    q.push(s);
+    hei[s] = 0;
+    q.push(src);
  
     while(!q.empty()) {
         int u = q.front(); 
         q.pop(); 
  
-        for(int v : g[u]) {
+        for(int v : adj[u]) {
             if (!vis[v]) {
                 vis[v] = 1; 
-                h[v] = h[u] + 1; 
+                hei[v] = hei[u] + 1; 
                 up[v][0] = u; 
                 q.push(v); 
             }
@@ -44,12 +45,12 @@ void dfs(int u, int par = 0) {
     }
 }
  
-int jump(int u, int v) {
-    if (h[u] != h[v]) {
-        if (h[u] < h[v]) swap(u, v); 
-        int k = h[u] - h[v];
-        for(int i = 0; (1 << i) <= k; ++i) {
-            if (k & (1 << i)) {
+int lca(int u, int v) {
+    if (hei[u] != hei[v]) {
+        if (hei[u] < hei[v]) swap(u, v); 
+        int dist = hei[u] - hei[v];
+        for(int i = 0; (1 << i) <= dist; ++i) {
+            if (dist & (1 << i)) {
                 u = up[u][i];
                 if (u == 0) return -1;
             }
@@ -57,7 +58,7 @@ int jump(int u, int v) {
     }
     if (u == v) return u;
  
-    int k = (int)__lg(h[u]); 
+    int k = (int)__lg(hei[u]); 
     for (int j = k; j >= 0; --j)
         if (up[u][j] != up[v][j]) 
             u = up[u][j], v = up[v][j];
@@ -68,28 +69,28 @@ int main() {
     ios_base::sync_with_stdio(0); 
     cin.tie(0); cout.tie(0); 
     
-    int n, q; cin >> n >> q; 
+    int n, quer; cin >> n >> quer; 
  
     for(int i = 2; i <= n; ++i) {
         int u, v; 
         cin >> u >> v;
  
-        g[u].push_back(v); 
-        g[v].push_back(u); 
+        adj[u].push_back(v); 
+        adj[v].push_back(u); 
     }
     bfs(1); 
-    for(int j = 1; j < log; ++j) 
+    for(int j = 1; j < LOG; ++j) 
         for(int u = 1; u <= n; ++u) 
             up[u][j] = up[up[u][j - 1]][j - 1];
  
     int visited[n + 1]; 
     fill_n(cnt, N, 0); 
     fill_n(visited, n + 1, 0); 
-    while(q--) {
+    while(quer--) {
         int u, v; 
         cin >> u >> v; 
  
-        int c = jump(u, v); 
+        int c = lca(u, v); 
         visited[c]++;
         cnt[u]++; 
         cnt[v]++; 
