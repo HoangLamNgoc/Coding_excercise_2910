2f4909d61dd94e357648c256545d4aaa74d95ab2
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-11-20 22:34:13 +0700

    Create Edit Distance (Biến đổi xâu).cpp

---
+++ Dynamic Programming/CSES/Edit Distance (Biến đổi xâu).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std; 
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0); 
+
+    string stra, strb; cin >> stra >> strb; 
+
+    int alen = (int)stra.size();
+    int blen = (int)strb.size(); 
+    stra = "-" + stra; 
+    strb = "-" + strb; 
+
+    int L[alen + 1][blen + 1];
+    for(int i = 1; i <= alen; ++i) {
+        for(int j = 1; j <= blen; ++j) {
+            L[i][j] = 0; 
+        }
+    }
+    for(int i = 1; i <= alen; ++i) L[i][0] = i; 
+    for(int j = 1; j <= blen; ++j) L[0][j] = j;
+    for(int i = 1; i <= alen; ++i) {
+        for(int j = 1; j <= blen; ++j) {
+            if (stra[i] == strb[i]) 
+                L[i][j] = L[i - 1][j - 1]; 
+            else 
+                L[i][j] = max(L[i - 1][j - 1], L[i - 1][j], L[i][j - 1]) + 1; 
+        }
+    }
+    cout << L[alen][blen]; 
+}
