dc34eba2ae0cc9feac8814a3ff9e457283801398
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2026-02-22 11:08:41 +0700

    Create XORSUM Queries.cpp

---
+++ BIT/XORSUM Queries.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+// xorsum [u, v]
+using namespace std; 
+
+int n; 
+
+    vector<long long> sum; 
+
+    long long get_sum(int u) {
+        long long res = 0; 
+        while (u > 0) {
+            res ^= sum[u]; 
+            u -= (u & -u); 
+        }
+        return res; 
+    }
+
+    void update(int u, int val) {
+        int x = u; 
+        while (x <= n) {
+            sum[x] ^= val; 
+            x += (x & -x); 
+        }
+    }
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr); 
+
+    int q; 
+    cin >> n >> q; 
+
+    sum.assign(n + 1, 0); 
+    vector<int> arr(n + 1, 0); 
+    for(int i = 1; i <= n; ++i) {
+        int a; 
+        cin >> a; 
+
+        arr[i] = a; 
+        update(i, a);
+    }
+
+    while (q--) {
+        int u, v; 
+        cin >> u >> v; 
+        cout << (get_sum(v) ^ get_sum(u - 1)) << '\n';
+    }
+}
