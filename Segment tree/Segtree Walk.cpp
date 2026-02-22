1e4ab8d5272ba3790a008d32b273b9ac49970b65
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2026-02-22 17:39:48 +0700

    Create Segtree Walk.cpp

---
+++ Segment tree/Segtree Walk.cpp
@@ -0,0 +1,20 @@
+void build()
+{
+    for(int id = N + 1; id <= (N<<1); ++id) ST[id] = 1;
+    for(int id = N; id; --id) ST[id] = ST[(id<<1)] + ST[(id<<1)|1];
+}
+
+void kill(int pos)
+{
+    for(int id = pos; id; id >>= 1) --ST[id];
+}
+
+int cdbgv(int k) //kth alive
+{
+    for(int id = 1; id <= N;)
+    {
+        if(ST[(id<<1)] >= k) id <<= 1;
+        else k -= ST[(id<<1)]; id = (id<<1)|1;
+    }
+    return id - N;
+}
