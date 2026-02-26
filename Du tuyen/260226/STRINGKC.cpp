b8fbdfe5d323017bd06b5528d6dba464e410a375
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2026-02-26 15:37:36 +0700

    Update STRINGKC.cpp

--- Du tuyen/260226/STRINGKC.cpp
+++ Du tuyen/260226/STRINGKC.cpp
@@ -1,5 +1,40 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+string check(int n, string z, bool fh) {
+    int m = n / 2;
+    string target, source;
+    int si, ti, skip = 0;
+    string res = "";
+
+    if (fh) {
+        target = z.substr(n - m, m);
+        si = 0; ti = 0;
+        while (ti < m && si <= m) {
+            if (z[si] == target[ti]) {
+                si++; ti++;
+            } else {
+                if (skip++) break;
+                si++;
+            }
+        }
+        return (ti == m) ? target : "";
+    } else {
+        target = z.substr(0, m);
+        si = m; ti = 0;
+
+        while (ti < m && si < n) {
+            if (z[si] == target[ti]) {
+                si++; ti++;
+            } else {
+                if (skip++) break;
+                si++;
+            }
+        }
+        return (ti == m) ? target : "";
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -8,8 +43,7 @@ int main() {
     freopen("STRINGKC.INP", "r", stdin);
     freopen("STRINGKC.OUT", "w", stdout);
 
-    int n;
-    cin >> n;
+    int n; cin >> n;
 
     string z;
     cin >> z;
@@ -19,21 +53,17 @@ int main() {
         return 0;
     }
 
-    int m = (n - 1) / 2;
+    string s1 = check(n, z, true);
+    string s2 = check(n, z, false);
 
-    string s1 = z.substr(0, m);
-    string s2 = z.substr(m, m);
-
-    string s3 = z.substr(1, m);
-    string s4 = z.substr(m + 1, m);
-
-    bool ok1 = (s1 == s2);
-    bool ok2 = (s3 == s4);
-
-    if (ok1 && ok2) {
-        cout << "Multiple Solutions";
+    if (s1 == "" && s2 == "") {
+        cout << "No Solution";
+    } else if (s1 != "" && s2 != "") {
+        if (s1 == s2) cout << s1;
+        else cout << "Multiple Solutions";
+    } else {
+        cout << (s1 != "" ? s1 : s2);
     }
-    else if (ok1) cout << s1;
-    else if (ok2) cout << s3;
-    else cout << "No Solution";
+
+    return 0;
 }
